7275e35693992c068d64ffb89658615115d34998
Joel Palmer <joelpalmer@gmail.com>
2019-07-09 07:27:19 -0500

    add comments/steps to insertion sort

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
-/// Insertion sort
+/// Insertion sort (swapping)
 pub fn insertion_sort<T: Ord>(list: &mut [T]) {
+    // visit each element in the list
     for i in 1..list.len() {
+        // make a copy of the list index
         let mut j = i;
+        // while the element behind you is bigger, swap places
         while j > 0 && list[j - 1] > list[j] {
             list.swap(j - 1, j);
             j -= 1;
