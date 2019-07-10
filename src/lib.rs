616e4473a488e821d1108bce605ea99503776a23
Joel Palmer <joelpalmer@gmail.com>
2019-07-10 12:09:15 -0500

    refactored merge_sort

--- src/lib.rs
+++ src/lib.rs
@@ -17,53 +17,53 @@ pub fn insertion_sort<T: Ord>(list: &mut [T]) {
 }
 
 /// Merge Sort
-// Merges 2 ordered sub-lists. Sub-lists are differentiated
-// by a start, middle & end index within a primary mutable list, 'l1'.
-// Ordering result is copied to the worker list, 'l2'
-fn merge(l1: &Vec<i32>, s: usize, m: usize, e: usize, l2: &mut Vec<i32>) {
-    let mut ptr1 = s;
-    let mut ptr2 = m;
+// Perform merge sort on a mutable vector of 'i32' elements.
+pub fn merge_sort(list: &mut Vec<i32>) {
+    let size: usize = list.len();
+    let mut worker: Vec<i32> = vec![0; size];
+    split(list, 0, size, &mut worker);
 
-    for i in s..e {
-        // Continue to compare within each sub-list until 1 sub-list
-        // is exhausted. If a sub-list is exhausted, then the remaining elements
-        // in the other list are copied over, assuming they're already in order.
-        if (ptr1 < m) && (ptr2 >= e || l1[ptr1] <= l1[ptr2]) {
-            l2[i] = l1[ptr1];
-            ptr1 += 1;
-        } else {
-            l2[i] = l1[ptr2];
-            ptr2 += 1;
+    // Merges 2 ordered sub-lists. Sub-lists are differentiated
+    // by a start, middle & end index within a primary mutable list, 'primary'.
+    // Ordering result is copied to the worker list, 'worker'
+    fn merge(primary: &Vec<i32>, start: usize, mid: usize, end: usize, worker: &mut Vec<i32>) {
+        let mut ptr1 = start;
+        let mut ptr2 = mid;
+
+        for i in start..end {
+            // Continue to compare within each sub-list until 1 sub-list
+            // is exhausted. If a sub-list is exhausted, then the remaining elements
+            // in the other list are copied over, assuming they're already in order.
+            if (ptr1 < mid) && (ptr2 >= end || primary[ptr1] <= primary[ptr2]) {
+                worker[i] = primary[ptr1];
+                ptr1 += 1;
+            } else {
+                worker[i] = primary[ptr2];
+                ptr2 += 1;
+            }
         }
     }
-}
 
-// Copies all elements from a worker mutable list, 'l2', to a mutable primary
-// list, 'l1'. Index ranges are regarded to represent an ordered sublist
-// within the worker list, 'l2'.
-fn merge_copy(l1: &mut Vec<i32>, s: usize, e: usize, l2: &Vec<i32>) {
-    (s..e).for_each(|i| l1[i] = l2[i]);
-}
-// Splits a mutable list into 2 sub-lists. Split is done recursively
-// until only n sub-lists remain where n is the total number of elements in
-// the original list.
-// These sub-lists are then merged together, keeping order.
-fn merge_split(l1: &mut Vec<i32>, s: usize, e: usize, l2: &mut Vec<i32>) {
-    if e - s > 1 {
-        let m: usize = (e + s) / 2;
-
-        merge_split(l1, s, m, l2); // left
-        merge_split(l1, m, e, l2); // right
-        merge(l1, s, m, e, l2);
-        merge_copy(l1, s, e, l2);
+    // Copies all elements from a worker mutable list, 'worker', to a mutable primary
+    // list, 'primary'. Index ranges are regarded to represent an ordered sublist
+    // within the worker list, 'worker'.
+    fn copy(primary: &mut Vec<i32>, start: usize, end: usize, worker: &Vec<i32>) {
+        (start..end).for_each(|i| primary[i] = worker[i]);
     }
-}
+    // Splits a mutable list into 2 sub-lists. Split is done recursively
+    // until only n sub-lists remain where n is the total number of elements in
+    // the original list.
+    // These sub-lists are then merged together, keeping order.
+    fn split(primary: &mut Vec<i32>, start: usize, end: usize, worker: &mut Vec<i32>) {
+        if end - start > 1 {
+            let mid: usize = (end + start) / 2;
 
-// Perform merge sort on a mutable vector of 'i32' elements.
-pub fn sort(l: &mut Vec<i32>) {
-    let size: usize = l.len();
-    let mut worker: Vec<i32> = vec![0; size];
-    merge_split(l, 0, size, &mut worker);
+            split(primary, start, mid, worker); // left
+            split(primary, mid, end, worker); // right
+            merge(primary, start, mid, end, worker);
+            copy(primary, start, end, worker);
+        }
+    }
 }
 #[cfg(test)]
 mod tests {
@@ -79,7 +79,7 @@ mod tests {
     #[test]
     fn test_merge_sort() {
         let mut l = vec![3, 1, 5, 4, 2];
-        sort(&mut l);
+        merge_sort(&mut l);
         assert_eq!(l, vec![1, 2, 3, 4, 5]);
     }
 }
